be6ec695cc4fb1a00d569336606d232c3c327c20
xgh <411604658@qq.com>
2019-11-26 14:56:35 +0800

    123

--- src/components/public/Footer.vue
+++ src/components/public/Footer.vue
@@ -1,11 +1,35 @@
 <template>
-    <div>
-        7777777777777777777777888
+    <div class="page_vessel footerBod">
+        <el-row :gutter="20">
+          <el-col :span="8" style="border: 1px solid red;">
+            <div class="grid-content bg-purple">
+                <!-- logo -->
+                <div class="footerLogo">
+                  <img src="../../assets/images/icon/projectLogo_1.png">
+                </div>
+                <!-- 公司名称-->
+                <div class="companyTitle">
+                  <label>创世代资产交易平台</label>
+                </div>
+                <!-- 微信、微博二维码-->
+                <div class="social">
+                  <a href="javascript://" class="wxIcon"></a>
+                  <a href="javascript://" class="wbIcon"></a>
+                </div>
+                <!-- 备案号-->
+                <div class="varchar">
+                  <label>上海绝顶服务有限公司</label>
+                  <label>沪ICP备17025462号</label>
+                </div>
+            </div>
+          </el-col>
+          <el-col :span="16" style="border: 1px solid red;"><div class="grid-content bg-purple">2</div></el-col>
+        </el-row>
     </div>
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
--- src/components/public/Head.vue
+++ src/components/public/Head.vue
@@ -1,74 +1,76 @@
 <template>
-    <div class="page_vessel headBod">
-      <!-- logo-->
-      <a href="javascript://" class="headLogo">
-        <img src="../../assets/images/icon/projectLogo_1.png">
-      </a>
-      <!-- nav-->
-      <div class="headNav">
-        <a href="javascript://" class="navHover">首页</a>
-        <a href="javascript://">交易专区</a>
-        <a href="javascript://">新闻资讯</a>
-        <a href="javascript://">企业公告</a>
-        <a href="javascript://">上币申请</a>
-        <a href="javascript://">客户端下载</a>
+  <div class="page_vessel headBod">
+    <!-- logo-->
+    <a href="javascript://" class="headLogo">
+      <img src="../../assets/images/icon/projectLogo_1.png">
+    </a>
+    <!-- nav-->
+    <div class="headNav">
+      <a href="javascript://" class="navHover">首页</a>
+      <a href="javascript://">交易专区</a>
+      <a href="javascript://">新闻资讯</a>
+      <a href="javascript://">企业公告</a>
+      <a href="javascript://">上币申请</a>
+      <a href="javascript://">客户端下载</a>
+    </div>
+    <!-- 用户信息-->
+    <div class="userList">
+      <!-- 未登录 -->
+      <div class="userHide" style="display: none;">
+        <a href="javascript://">登录</a>
+        <a href="javascript://" class="bg_0">注册</a>
       </div>
-      <!-- 用户信息-->
-      <div class="userList">
-          <!-- 未登录 -->
-          <div class="userHide" style="display: none;">
-            <a href="javascript://">登录</a>
-            <a href="javascript://" class="bg_0">注册</a>
+      <!-- 已登录-->
+      <div class="userShow">
+        <a href="javascript://" class="userName">我的账户<i class="el-icon-caret-bottom"></i></a>
+        <!-- 用户信息弹出框-->
+        <div class="userInfo">
+          <div class="residual">
+            <a href="javascript://" class="redHover">持仓</a>
+            <a href="javascript://">保证金</a>
+            <p class="color_0">
+              <font>¥</font><label>0.00</label>
+            </p>
           </div>
-          <!-- 已登录-->
-          <div class="userShow">
-            <a href="javascript://" class="userName">我的账户<i class="el-icon-caret-bottom"></i></a>
-            <!-- 用户信息弹出框-->
-            <div class="userInfo">
-                <div class="residual">
-                  <a href="javascript://" class="redHover">持仓</a>
-                  <a href="javascript://">保证金</a>
-                  <p class="color_0"><font>¥</font><label>0.00</label></p>
-                </div>
-                <div class="recharge">
-                  <a href="javascript://" class="bg_0">充值</a>
-                  <a href="javascript://" class="color_0">提现</a>
-                </div>
-                <div class="navEj">
-                    <ul>
-                      <li><a href="javascript://">我的账户</a></li>
-                      <li><a href="javascript://">交易记录</a></li>
-                      <li><a href="javascript://">我要开户</a></li>
-                      <li><a href="javascript://">邀请记录</a></li>
-                    </ul>
-                </div>
-                <div class="infoBtn">
-                    <a href="javascript://">安全设置</a>
-                    <a href="javascript://">邀请码</a>
-                    <a href="javascript://">退出</a>
-                </div>
-            </div>
+          <div class="recharge">
+            <a href="javascript://" class="bg_0">充值</a>
+            <a href="javascript://" class="color_0">提现</a>
           </div>
-          <a href="javascript://" class="userDown">下载</a>
-          <!-- 下载二维码 -->
-          <div class="downEwm" style="display:none ;">
-            <div>
-              <img src="../../assets/images/img/ewm_img.png">
-              <label>Android</label>
-            </div>
-            <div>
-              <img src="../../assets/images/img/ewm_img.png">
-              <label>IOS</label>
-            </div>
+          <div class="navEj">
+            <ul>
+              <li><a href="javascript://">我的账户</a></li>
+              <li><a href="javascript://">交易记录</a></li>
+              <li><a href="javascript://">我要开户</a></li>
+              <li><a href="javascript://">邀请记录</a></li>
+            </ul>
           </div>
-          <a href="javascript://" class="userEn">简体中文<i class="el-icon-caret-bottom"></i></a>
-          <!-- 国际化 -->
-          <div class="enGjh" style="display:none ;">
-            <label>简体中文</label>
-            <label>English</label>
+          <div class="infoBtn">
+            <a href="javascript://">安全设置</a>
+            <a href="javascript://">邀请码</a>
+            <a href="javascript://">退出</a>
           </div>
+        </div>
       </div>
-        <!-- <el-select @change="langChange" placeholder="请选择" v-model='value'>
+      <a href="javascript://" class="userDown">下载</a>
+      <!-- 下载二维码 -->
+      <div class="downEwm" style="display:none ;">
+        <div>
+          <img src="../../assets/images/img/ewm_img.png">
+          <label>Android</label>
+        </div>
+        <div>
+          <img src="../../assets/images/img/ewm_img.png">
+          <label>IOS</label>
+        </div>
+      </div>
+      <a href="javascript://" class="userEn">简体中文<i class="el-icon-caret-bottom"></i></a>
+      <!-- 国际化 -->
+      <div class="enGjh" style="display:none ;">
+        <label>简体中文</label>
+        <label>English</label>
+      </div>
+    </div>
+    <!-- <el-select @change="langChange" placeholder="请选择" v-model='value'>
             <el-option
                 v-for="item in options"
                 :key="item.value"
@@ -76,35 +78,33 @@
                 :value="item.value">
             </el-option>
         </el-select> -->
-    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    data () {
-        return {
-            options:[
-                {
-                value: 'zh',
-                label: '中文'
-                }, {
-                value: 'en',
-                label: 'English'
-                }
-            ],
-            value:localStorage.lang || 'zh'
-        }
+  export default {
+    data() {
+      return {
+        options: [{
+          value: 'zh',
+          label: '中文'
+        }, {
+          value: 'en',
+          label: 'English'
+        }],
+        value: localStorage.lang || 'zh'
+      }
     },
     methods: {
-        //语言切换
-        langChange(e){
-            localStorage.setItem('lang',e);
-            this.value = e;
-            this.$i18n.locale = e;
-//          window.location.reload()
-        }
+      //语言切换
+      langChange(e) {
+        localStorage.setItem('lang', e);
+        this.value = e;
+        this.$i18n.locale = e;
+        //          window.location.reload()
+      }
     }
-}
+  }
 </script>
 <style>
   @import '../../assets/css/public.css';
