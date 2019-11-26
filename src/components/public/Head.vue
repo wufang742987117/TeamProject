162f335ae56e6b560a8a33e5d23cc522c4c513b4
方武 <742987117@qq.com>
2019-11-26 14:35:10 +0800

    头部翻译修改

--- src/components/public/Head.vue
+++ src/components/public/Head.vue
@@ -6,51 +6,52 @@
       </a>
       <!-- nav-->
       <div class="headNav">
-        <a href="javascript://" class="navHover">首页</a>
-        <a href="javascript://">交易专区</a>
-        <a href="javascript://">新闻资讯</a>
-        <a href="javascript://">企业公告</a>
-        <a href="javascript://">上币申请</a>
-        <a href="javascript://">客户端下载</a>
+        <a href="javascript://" class="navHover">{{$t('message.HEAD_INDEX')}}</a>
+        <a href="javascript://">{{$t('message.HEAD_TRADE')}}</a>
+        <a href="javascript://">{{$t('message.HEAD_NEWS')}}</a>
+        <a href="javascript://">{{$t('message.HEAD_NOTICE')}}</a>
+        <a href="javascript://">{{$t('message.HEAD_APPLY')}}</a>
+        <a href="javascript://">{{$t('message.HEAD_DOWN')}}</a>
       </div>
       <!-- 用户信息-->
       <div class="userList">
           <!-- 未登录 -->
-          <div class="userHide" style="display: none;">
-            <a href="javascript://">登录</a>
-            <a href="javascript://" class="bg_0">注册</a>
+          <div class="userHide" v-show='!token'>
+            <a href="javascript://" @click='login()'>{{$t('message.HEAD_LOGIN')}}</a>
+            <a href="javascript://" class="bg_0">{{$t('message.HEAD_REGISTER')}}</a>
           </div>
           <!-- 已登录-->
-          <div class="userShow">
-            <a href="javascript://" class="userName">我的账户<i class="el-icon-caret-bottom"></i></a>
+          <div class="userShow" v-show='token'>
+            <a href="javascript://" class="userName">{{$t('message.HEAD_ACCOUNT')}}<i class="el-icon-caret-bottom"></i></a>
             <!-- 用户信息弹出框-->
             <div class="userInfo">
                 <div class="residual">
-                  <a href="javascript://" class="redHover">持仓</a>
-                  <a href="javascript://">保证金</a>
-                  <p class="color_0"><font>¥</font><label>0.00</label></p>
+                  <a href="javascript://" @click="setActive(1)" :class="{'redHover':isActive==1}">{{$t('message.HEAD_CHIZCANG')}}</a>
+                  <a href="javascript://" @click="setActive(2)" :class="{'redHover':isActive==2}">{{$t('message.HEAD_BZJ')}}</a>
+                  <p class="color_0" v-show='isActive==1'><font>¥</font><label>0.00</label></p>
+                  <p class="color_0" v-show='isActive==2'><font>¥</font><label>3.33</label></p>
                 </div>
                 <div class="recharge">
-                  <a href="javascript://" class="bg_0">充值</a>
-                  <a href="javascript://" class="color_0">提现</a>
+                  <a href="javascript://" class="bg_0">{{$t('message.HEAD_RECHARGE')}}</a>
+                  <a href="javascript://" class="color_0">{{$t('message.HEAD_WITHDRAW')}}</a>
                 </div>
                 <div class="navEj">
                     <ul>
-                      <li><a href="javascript://">我的账户</a></li>
-                      <li><a href="javascript://">交易记录</a></li>
-                      <li><a href="javascript://">我要开户</a></li>
-                      <li><a href="javascript://">邀请记录</a></li>
+                      <li><a href="javascript://">{{$t('message.HEAD_ACCOUNT')}}</a></li>
+                      <li><a href="javascript://">{{$t('message.HEAD_TRADE_HISTORY')}}</a></li>
+                      <li><a href="javascript://">{{$t('message.HEAD_KAIHU')}}</a></li>
+                      <li><a href="javascript://">{{$t('message.HEAD_YAOQING')}}</a></li>
                     </ul>
                 </div>
                 <div class="infoBtn">
-                    <a href="javascript://">安全设置</a>
-                    <a href="javascript://">邀请码</a>
-                    <a href="javascript://">退出</a>
+                    <a href="javascript://">{{$t('message.HEAD_SETTING')}}</a>
+                    <a href="javascript://">{{$t('message.HEAD_CODE')}}</a>
+                    <a href="javascript://" @click='goOut()'>{{$t('message.HEAD_OUT')}}</a>
                 </div>
             </div>
           </div>
           <div class="down-wrap" @mouseover="showDownTp()" @mouseout="hideDownTp()">
-          	<a href="javascript://" class="userDown" >下载</a>
+          	<a href="javascript://" class="userDown" >{{$t('message.HEAD_DOWNWRAP')}}</a>
           </div>
           
           <!-- 下载二维码 -->
@@ -64,24 +65,18 @@
               <label>IOS</label>
             </div>
           </div>
-          <div class="down-wrap">
-          	 <a href="javascript://" class="userEn">简体中文<i class="el-icon-caret-bottom"></i></a>
+          <div class="down-wrap" @mouseover="showTranslateTp()" @mouseout="hideTranslateTp()">
+          	 <a href="javascript://" class="userEn">
+          	 	{{language | languageStatus}}
+          	 	<i class="el-icon-caret-bottom"></i></a>
           </div>
          
           <!-- 国际化 -->
-          <div class="enGjh" style="display:none ;">
-            <label>简体中文</label>
-            <label>English</label>
+          <div class="enGjh" v-show="translate" @mouseover="showTranslateBt()" @mouseout="hideTranslateBt()">
+            <label :class="{'active':language=='zh'}"  @click="langChange('zh')">简体中文</label>
+            <label :class="{'active':language=='en'}" @click="langChange('en')">English</label>
           </div>
       </div>
-        <!-- <el-select @change="langChange" placeholder="请选择" v-model='value'>
-            <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value">
-            </el-option>
-        </el-select> -->
     </div>
 </template>
 
@@ -101,14 +96,28 @@ export default {
             down:false,
             downTop:false,
             downBot:false,
-            value:localStorage.lang || 'zh'
+            translate:false,
+            translateTop:false,
+            translateBot:false,
+            language:localStorage.lang || 'zh',
+            token:false,
+            isActive:1
         }
+    },
+    filters:{
+    	languageStatus(type) {
+    		if(type == 'zh') {
+	          return '简体中文'
+	        } else if(type == 'en') {
+	          return 'English'
+	        }
+    	}
     },
     methods: {
         //语言切换
         langChange(e){
             localStorage.setItem('lang',e);
-            this.value = e;
+            this.language = e;
             this.$i18n.locale = e;
 //          window.location.reload()
         },
@@ -131,6 +140,36 @@ export default {
         	if(this.downTop==false) {
         		this.down = false;
         	}
+        },
+        
+        showTranslateTp(){
+        	this.translateTop = true;
+        	this.translate = true;
+        },
+        showTranslateBt(){
+        	this.translateBot = true;
+        	this.translate = true;
+        },
+        hideTranslateTp(){
+        	this.translateTop =false
+        	if(this.translateBot==false) {
+        		this.translate = false;
+        	}
+        },
+        hideTranslateBt(){
+        	this.translateBot = false;
+        	if(this.translateTop==false) {
+        		this.translate = false;
+        	}
+        },
+        login(){
+        	this.token = true;
+        },
+        goOut(){
+        	this.token = false;
+        },
+        setActive(i) {
+        	this.isActive = i;
         }
         
     }
